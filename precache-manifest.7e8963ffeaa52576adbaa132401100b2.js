5d3231a8b165643d3ccfcbdccab60d9103a4de9c
Julia Philipps <10884041+jphili@users.noreply.github.com>
2018-11-11 15:01:10 +0200

    Updates

---
+++ precache-manifest.7e8963ffeaa52576adbaa132401100b2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/local-weather/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "449eefe68be63dfa7ef5",
+    "url": "/local-weather/static/css/main.01e2c8f6.chunk.css"
+  },
+  {
+    "revision": "a7d1a219f62cad06243e",
+    "url": "/local-weather/static/js/1.a7d1a219.chunk.js"
+  },
+  {
+    "revision": "b77c6adc735e8f88e3c2",
+    "url": "/local-weather/static/js/runtime~main.b77c6adc.js"
+  },
+  {
+    "revision": "be2092c795e7372b3c891ad5958f4b4d",
+    "url": "/local-weather/static/media/background.be2092c7.svg"
+  },
+  {
+    "revision": "449eefe68be63dfa7ef5",
+    "url": "/local-weather/static/js/main.449eefe6.chunk.js"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/local-weather/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/local-weather/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/local-weather/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/local-weather/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "fe1596af424e309af9d02c843e61e3d1",
+    "url": "/local-weather/index.html"
+  }
+];
